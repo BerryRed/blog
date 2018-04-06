3e1810655127ad37e6f63a3db73399fd301c84b8
Berry <berry_rednes@mail.ru>
2018-04-06 23:15:23 +0700

    new style form for the best blog 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
   def index
+  end
 
+  def new
+    end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
   end
 end
-
