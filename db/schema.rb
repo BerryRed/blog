f3bb98b2e3c1b26108fa533085aa94d8dcf69a15
Berry <berry_rednes@mail.ru>
2018-04-09 21:31:37 +0700

    Make comments and delete comments ^ ^  : З

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,12 @@
 ActiveRecord::Schema.define(version: 20180409123142) do
 
   create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
