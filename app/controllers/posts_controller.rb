9f72cfdfb279a12e7f32ee317ba9eeee429e6c2f
Berry <berry_rednes@mail.ru>
2018-04-09 19:14:30 +0700

    somethink

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,8 +21,29 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+      end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :body)
     end
-end
+  end
